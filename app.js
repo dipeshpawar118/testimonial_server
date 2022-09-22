9427b6fcf91ca9fb2de3e7902eb578f97939a0bc
dipeshpawar118 <dipeshpawar118@gmail.com>
2022-09-22 23:12:51 +0530

    cloud db config

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express();
 const database = require('./config/db.config')
 const port = 3000
-
+const cors = require('cors');
 const testimonialRoute =  require('./routes/testimonialRoute')
 database();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE" ,
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  allowedHeaders:"Origin, X-Requested-With, Content-Type, Accept, loggedinuseruid"
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
 app.use('/testimonial' , testimonialRoute)
--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoString = "mongodb://localhost:27017/testimonial";
-
+const mongoString = "mongodb+srv://rohit:ionic%4012345@dndevcluster.k7fvkpb.mongodb.net/defineNext?retryWrites=true&w=majority";
+// const mongoString = "mongodb://localhost:27017/testimonial";
 
 const connectDataBase = () =>{
     mongoose.connect(mongoString);
