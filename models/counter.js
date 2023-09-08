084344239539676df747559cf286aa01636d4704
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-08 11:13:11 +0530

    remove all project

--- models/counter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CounterSchema = new mongoose.Schema({
-    count: { type: Number ,  default: 0 , required : true }
-});
-
-const Counter = mongoose.model('Counter', CounterSchema);
-module.exports = Counter;
